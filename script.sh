6af76ff65a6926fade4f3f9b60750bd0c15e7a1a
ZaidanAgusta <zaidanagusta319@gmail.com>
2025-10-07 16:50:15 +0700

    #repo

--- script.sh
+++ script.sh
@@ -5,10 +5,10 @@ rm -rf .repo/local_manifests/
 rm -rf prebuilts/clang/host/linux-x86
 
 # Initialize ROM manifest
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
+#repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
 
 # Sync the repo with force to ensure a clean sync
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 
 # Set up th build environment
 . build/envsetup.sh
