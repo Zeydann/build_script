17df92f6551eb0d1737cd5c53c3d950277ccf4c3
ZaidanAgusta <zaidanagusta319@gmail.com>
2025-10-08 16:08:34 +0700

    test

--- script.sh
+++ script.sh
@@ -5,10 +5,10 @@ rm -rf .repo/local_manifests/
 rm -rf prebuilts/clang/host/linux-x86
 
 # Initialize ROM manifest
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+#repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 
 # Sync the repo with force to ensure a clean sync
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 
 # Set up th build environment
 . build/envsetup.sh
