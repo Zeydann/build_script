215532f24c1c07a3a9848e54a6ddb6a26ed701d9
ZaidanAgusta <zaidanagusta319@gmail.com>
2025-10-12 06:41:10 +0700

    fix

--- script.sh
+++ script.sh
@@ -4,16 +4,19 @@
 rm -rf .repo/local_manifests/
 
 # Initialize ROM manifest
-repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
+#repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 
 # Viper
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
+#git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
 
 # B
-cd frameworks/native && curl https://github.com/pos-xyz-personal/frameworks_native/commit/9b1091664329d9b8055d246210cf442b8d7eb5da.patch | git am && cd ../..
+#cd frameworks/native && curl https://github.com/pos-xyz-personal/frameworks_native/commit/9b1091664329d9b8055d246210cf442b8d7eb5da.patch | git am && cd ../..
+
+# Set up th build environment
+. build/envsetup.sh
 
 # Choose the target device
 riseup rodin userdebug
