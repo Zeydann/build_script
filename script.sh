36036e78f55b0d4cdbc7ec453a03739fe3ce6cf6
ZaidanAgusta <180064682+Zeydann@users.noreply.github.com>
2025-10-23 04:36:19 +0700

    test

--- script.sh
+++ script.sh
@@ -5,6 +5,3 @@ git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages
 
 # ytta
 cd frameworks/native && curl https://github.com/pos-xyz-personal/frameworks_native/commit/9b1091664329d9b8055d246210cf442b8d7eb5da.patch | git am && cd ../..
-
-# keys
-git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys && cd vendor/infinity-priv/keys && ./keys.sh && cd ../../..
