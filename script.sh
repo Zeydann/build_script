c2ed84b8512ba60593fbffef26f9e4939addeed3
ZaidanAgusta <zeydann.sc@gmail.com>
2025-10-29 20:16:32 +0700

    test

--- script.sh
+++ script.sh
@@ -4,13 +4,13 @@
 rm -rf .repo/local_manifests/
 
 # Initialize ROM manifest
-#repo init -u https://github.com/LumineDroid/platform_manifest.git -b bynx --git-lfs
+repo init -u https://github.com/LumineDroid/platform_manifest.git -b bynx --git-lfs
 
 # Sync the repo with force to ensure a clean sync
-#/opt/crave/resync.sh
+/opt/crave/resync.sh
 
 # B
-#cd frameworks/native && curl https://github.com/pos-xyz-personal/frameworks_native/commit/9b1091664329d9b8055d246210cf442b8d7eb5da.patch | git am && cd ../..
+cd frameworks/native && curl https://github.com/pos-xyz-personal/frameworks_native/commit/9b1091664329d9b8055d246210cf442b8d7eb5da.patch | git am && cd ../..
 
 # Set up th build environment
 . build/envsetup.sh
