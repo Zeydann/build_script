c27b4f9527c440cc28ff14874cdef8dc458c719d
ZaidanAgusta <zeydann.sc@gmail.com>
2025-11-15 15:31:35 +0700

    gas [2]

--- script.sh
+++ script.sh
@@ -4,25 +4,25 @@
 rm -rf .repo/local_manifests/
 
 # Initialize ROM manifest
-repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
+#repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 
 # keys
-git clone https://github.com/Zeydann/android_vendor_private_keys vendor/private/keys
+#git clone https://github.com/Zeydann/android_vendor_private_keys vendor/private/keys
 
 # ViperFX
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
+#git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
 
 # B
-cd frameworks/native && curl https://github.com/pos-xyz-personal/frameworks_native/commit/9b1091664329d9b8055d246210cf442b8d7eb5da.patch | git am && cd ../..
+#cd frameworks/native && curl https://github.com/pos-xyz-personal/frameworks_native/commit/9b1091664329d9b8055d246210cf442b8d7eb5da.patch | git am && cd ../..
 
 # c
-rm -rf hardware/lineage/compat && rm -rf hardware/lineage/interfaces 
+#rm -rf hardware/lineage/compat && rm -rf hardware/lineage/interfaces 
 
 # d
-git clone https://github.com/LineageOS/android_hardware_lineage_compat.git -b lineage-22.2 hardware/lineage/compat && git clone https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-22.2 hardware/lineage/interfaces
+#git clone https://github.com/LineageOS/android_hardware_lineage_compat.git -b lineage-22.2 hardware/lineage/compat && git clone https://github.com/LineageOS/android_hardware_lineage_interfaces.git -b lineage-22.2 hardware/lineage/interfaces
 
 # Set up th build environment
 . build/envsetup.sh
