a473de7ea651db9b710b0b4962b85b2a23e5eb17
ZaidanAgusta <zeydann.sc@gmail.com>
2025-11-18 08:10:54 +0700

    test again

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ repo init -u https://github.com/Evolution-X/manifest -b bka-q1 --git-lfs
 #git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
 
 # B
-cd frameworks/native && curl https://github.com/pos-xyz-personal/frameworks_native/commit/9b1091664329d9b8055d246210cf442b8d7eb5da.patch | git am && cd ../..
+#cd frameworks/native && curl https://github.com/pos-xyz-personal/frameworks_native/commit/9b1091664329d9b8055d246210cf442b8d7eb5da.patch | git am && cd ../..
 
 # c
 #rm -rf hardware/lineage/compat && rm -rf hardware/lineage/interfaces 
